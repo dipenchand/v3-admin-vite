7f56e8497cb3e26bdbcfed2967d517a10c3ff43a
dipenchand <dipenchand@gmail.com>
2024-03-19 18:05:20 +0545

    Login functional

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,8 +3,7 @@ import { reactive, ref } from "vue"
 import { useRouter } from "vue-router"
 import { useUserStore } from "@/store/modules/user"
 import { type FormInstance, type FormRules } from "element-plus"
-import { User, Lock, Key, Picture, Loading } from "@element-plus/icons-vue"
-import { getLoginCodeApi } from "@/api/login"
+// import { getLoginCodeApi } from "@/api/login"
 import { type LoginRequestData } from "@/api/login/types/login"
 import ThemeSwitch from "@/components/ThemeSwitch/index.vue"
 import Owl from "./components/Owl.vue"
@@ -13,27 +12,19 @@ import { useFocus } from "./hooks/useFocus"
 const router = useRouter()
 const { isFocus, handleBlur, handleFocus } = useFocus()
 
-/** 登录表单元素的引用 */
 const loginFormRef = ref<FormInstance | null>(null)
-
-/** 登录按钮 Loading */
 const loading = ref(false)
-/** 验证码图片 URL */
-const codeUrl = ref("")
-/** 登录表单数据 */
 const loginFormData: LoginRequestData = reactive({
   username: "admin",
-  password: "12345678",
-  code: ""
+  password: "12345678"
 })
-/** 登录表单校验规则 */
+
 const loginFormRules: FormRules = {
-  username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+  username: [{ required: true, message: "Please enter email", trigger: "blur" }],
   password: [
-    { required: true, message: "请输入密码", trigger: "blur" },
-    { min: 8, max: 16, message: "长度在 8 到 16 个字符", trigger: "blur" }
-  ],
-  code: [{ required: true, message: "请输入验证码", trigger: "blur" }]
+    { required: true, message: "Please enter password", trigger: "blur" },
+    { min: 8, max: 16, message: "8 to 16 characters in length", trigger: "blur" }
+  ]
 }
 /** 登录逻辑 */
 const handleLogin = () => {
@@ -46,30 +37,16 @@ const handleLogin = () => {
           router.push({ path: "/" })
         })
         .catch(() => {
-          createCode()
           loginFormData.password = ""
         })
         .finally(() => {
           loading.value = false
         })
     } else {
-      console.error("表单校验不通过", fields)
+      console.error("Form verification failed", fields)
     }
   })
 }
-/** 创建验证码 */
-const createCode = () => {
-  // 先清空验证码的输入
-  loginFormData.code = ""
-  // 获取验证码
-  codeUrl.value = ""
-  getLoginCodeApi().then((res) => {
-    codeUrl.value = res.data
-  })
-}
-
-/** 初始化验证码 */
-createCode()
 </script>
 
 <template>
@@ -85,7 +62,7 @@ createCode()
           <el-form-item prop="username">
             <el-input
               v-model.trim="loginFormData.username"
-              placeholder="用户名"
+              placeholder="Email"
               type="text"
               tabindex="1"
               :prefix-icon="User"
@@ -95,7 +72,7 @@ createCode()
           <el-form-item prop="password">
             <el-input
               v-model.trim="loginFormData.password"
-              placeholder="密码"
+              placeholder="Password"
               type="password"
               tabindex="2"
               :prefix-icon="Lock"
@@ -105,33 +82,7 @@ createCode()
               @focus="handleFocus"
             />
           </el-form-item>
-          <el-form-item prop="code">
-            <el-input
-              v-model.trim="loginFormData.code"
-              placeholder="验证码"
-              type="text"
-              tabindex="3"
-              :prefix-icon="Key"
-              maxlength="7"
-              size="large"
-            >
-              <template #append>
-                <el-image :src="codeUrl" @click="createCode" draggable="false">
-                  <template #placeholder>
-                    <el-icon>
-                      <Picture />
-                    </el-icon>
-                  </template>
-                  <template #error>
-                    <el-icon>
-                      <Loading />
-                    </el-icon>
-                  </template>
-                </el-image>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-button :loading="loading" type="primary" size="large" @click.prevent="handleLogin">登 录</el-button>
+          <el-button :loading="loading" type="primary" size="large" @click.prevent="handleLogin">Log in</el-button>
         </el-form>
       </div>
     </div>
